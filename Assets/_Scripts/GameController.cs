53c941921f8284cf96fdb3491877df2e8c45e1ee
jagpreetjattana <jagpreet@hotmail.com>
2016-03-02 19:33:06 -0500

    Initial Commit

---
+++ Assets/_Scripts/GameController.cs
@@ -0,0 +1,106 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+	// PUBLIC INSTANCE VARIABLES
+	public int tankCount;
+	public GameObject tank;
+    public Text livesLabel;
+    public Text scoreLabel;
+    public Text gameOverLabel;
+    public Text finalScoreLabel;
+    public PlayerController player;
+    public Button restartButton;
+
+
+    
+    //private instance variables
+    private int _scoreValue;
+    private int _livesValue;
+
+
+
+    //getters and setters
+    public int ScoreValue
+    {
+        get
+        {
+            return _scoreValue;
+        }
+
+        set
+        {
+            _scoreValue = value;
+            this.scoreLabel.text = "Score: " + this._scoreValue;
+        }
+    }
+
+    public int LivesValue
+    {
+        get
+        {
+            return _livesValue;
+        }
+
+        set
+        {
+            _livesValue = value;
+            if (this._livesValue <= 0)
+            {
+                this._endGame();
+            }
+
+            else {
+                this.livesLabel.text = "Lives: " + this._livesValue;
+            }
+
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+		this._GenerateTanks ();
+        this._initialize();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	// generate Clouds
+	private void _GenerateTanks() {
+		for (int count=0; count < this.tankCount; count++) {
+			Instantiate(tank);
+		}
+	}
+
+    //private methods
+   
+    private void _initialize()
+    {
+        this.ScoreValue = 0;
+        this.LivesValue = 5;
+        this.gameOverLabel.enabled = false;
+        this.finalScoreLabel.enabled = false;
+        this.restartButton.gameObject.SetActive(false);
+    }
+    private void _endGame()
+    {
+        this.finalScoreLabel.text = "Final Score:" + this._scoreValue;
+        this.gameOverLabel.enabled = true;
+        this.finalScoreLabel.enabled = true;
+         this.restartButton.gameObject.SetActive(true);
+        this.player.gameObject.SetActive(false);
+        this.livesLabel.enabled = false;
+        this.scoreLabel.enabled = false;
+       
+
+    }
+    //Public Method
+    public void RestarButtonClicked()
+    {
+        Application.LoadLevel("Main");
+    }
+}
---
+++ Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	// PUBLIC INSTANCE VARIABLES +++++++++++++++++++++++++++++++++++++++
+	public float speed;
+	public Boundary boundary;
+    public GameController gameController;
+    public EnemyController enemy;
+
+    // get a reference to the camera to make mouse input work
+    public Camera camera; 
+	
+	// PRIVATE INSTANCE VARIABLES
+	private Vector2 _newPosition = new Vector2(0.0f, 0.0f);
+	
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		this._CheckInput ();
+	}
+
+	private void _CheckInput() {
+		this._newPosition = gameObject.GetComponent<Transform> ().position; // current position
+
+		// movement by keyboard
+		if (Input.GetAxis ("Horizontal") > 0) {
+			this._newPosition.x += this.speed; // add move value to current position
+		}
+	
+		
+		if (Input.GetAxis ("Horizontal") < 0) {
+			this._newPosition.x -= this.speed; // subtract move value to current position
+		}
+		
+
+		gameObject.GetComponent<Transform>().position = this._newPosition;
+	}
+
+    //used ontrigger method to detect collisions between objects
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+
+        if (other.gameObject.CompareTag("Tank"))
+        {
+         
+            this.gameController.LivesValue -= 1;
+       
+        }
+ 
+    }
+
+
+    private void _BoundaryCheck() {
+		if (this._newPosition.x < this.boundary.xMin) {
+			this._newPosition.x = this.boundary.xMin;
+		}
+
+		if (this._newPosition.x > this.boundary.xMax) {
+			this._newPosition.x = this.boundary.xMax;
+		}
+	}
+}
